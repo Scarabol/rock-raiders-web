e68647bb87d8260d8a17b4aa9875b3e238b13097
Scarabol <scarabol@gmail.com>
2020-10-09 20:59:23 +0200

    wip cfgparser

---
+++ src/game/engine/ResourceManager.js
@@ -0,0 +1,13 @@
+function ResourceManager() {
+
+}
+
+ResourceManager.prototype = {
+
+    loadResources(onProgress) {
+        // FIXME load all resources from WAD/other file
+    },
+
+}
+
+export { ResourceManager }
