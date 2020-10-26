930561effa9a5b5dbdd0d41735530d4b242562ef
Scarabol <scarabol@gmail.com>
2020-10-26 11:08:34 +0100

    Setup game engine after cfg loaded

--- src/main.ts
+++ src/main.ts
@@ -7,12 +7,28 @@ import { RewardScreen } from './screen/RewardScreen';
 // setup basic game engine structure
 
 const loadingScreen = new LoadingScreen();
-const mainMenuScreen = new MainMenuScreen();
-const gameScreen = new GameScreen();
-const rewardScreen = new RewardScreen();
 
 // link all components with callbacks
 
+ResourceManager.wadLoader.onLoad = () => {
+    // complete setup
+    const mainMenuScreen = new MainMenuScreen();
+    const gameScreen = new GameScreen();
+    const rewardScreen = new RewardScreen();
+
+    mainMenuScreen.onLevelSelected = (levelName) => {
+        gameScreen.startLevel(levelName);
+    };
+    gameScreen.onLevelEnd = (gameResult) => {
+        rewardScreen.showReward(gameResult);
+    };
+    rewardScreen.onContinue = mainMenuScreen.showLevelSelection;
+
+    // setup complete
+    loadingScreen.hide();
+    // mainMenuScreen.showMainMenu();
+    mainMenuScreen.selectLevel('Level02');
+};
 ResourceManager.wadLoader.onMessage = (msg: string) => {
     loadingScreen.setLoadingMessage(msg);
 };
@@ -22,18 +38,6 @@ ResourceManager.wadLoader.onInitialLoad = (totalResources: number) => {
 ResourceManager.wadLoader.onAssetLoaded = (assetIndex: number) => {
     loadingScreen.onAssetLoaded(assetIndex);
 };
-ResourceManager.wadLoader.onLoad = () => {
-    loadingScreen.hide();
-    // mainMenuScreen.showMainMenu();
-    mainMenuScreen.selectLevel('Level02');
-};
-mainMenuScreen.onLevelSelected = (levelName) => {
-    gameScreen.startLevel(levelName);
-};
-gameScreen.onLevelEnd = (gameResult) => {
-    rewardScreen.showReward(gameResult);
-};
-rewardScreen.onContinue = mainMenuScreen.showLevelSelection;
 
 // start the game engine with loading resources
 
