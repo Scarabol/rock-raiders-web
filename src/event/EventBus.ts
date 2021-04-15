98d1c1782f568570bdb5fdf717626344b46c90f4
Scarabol <scarabol@gmail.com>
2021-04-15 19:25:37 +0200

    Refactor event handling use Map instead of object

--- src/event/EventBus.ts
+++ src/event/EventBus.ts
@@ -2,22 +2,26 @@ import { removeFromArray } from '../core/Util'
 
 export class EventBus {
 
-    static eventListener = {} // FIXME use map
+    static eventListener = new Map<string, ((event: GameEvent) => any)[]>()
     static blockedEvents = []
 
     static publishEvent(event: GameEvent) {
         if (this.blockedEvents.includes(event.eventKey)) return // event is currently blocked from publishing
         if (!event.isLocal) console.log('Event published: ' + event.eventKey)
-        this.blockedEvents.push(event.eventKey);
-        (this.eventListener[event.eventKey] || []).forEach((callback) => callback(event)) // TODO add inheritance match by prefix
+        this.blockedEvents.push(event.eventKey)
+        this.getListener(event.eventKey).forEach((callback) => callback(event)) // TODO add inheritance match by prefix
         removeFromArray(this.blockedEvents, event.eventKey)
     }
 
     static registerEventListener(eventKey: string, callback: (GameEvent) => any) {
-        this.eventListener[eventKey] = this.eventListener[eventKey] || []
-        this.eventListener[eventKey].push(callback)
+        const listener = this.getListener(eventKey)
+        listener.push(callback)
+        this.eventListener.set(eventKey, listener)
     }
 
+    private static getListener(eventKey: string) {
+        return this.eventListener.get(eventKey) || []
+    }
 }
 
 export class GameEvent {
