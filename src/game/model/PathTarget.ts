5dd331496583f6d523a514094dd5343ae177f21d
Scarabol <scarabol@gmail.com>
2021-05-03 23:05:27 +0200

    Improve path target implementation

--- src/game/model/PathTarget.ts
+++ src/game/model/PathTarget.ts
@@ -1,4 +1,6 @@
 import { Vector2 } from 'three'
+import { RaiderActivity } from './activities/RaiderActivity'
+import { MaterialEntity } from './collect/MaterialEntity'
 
 export class PathTarget {
 
@@ -10,6 +12,18 @@ export class PathTarget {
 
     isInArea(position: Vector2): boolean {
         return false
+    }
+
+    canGatherItem(): boolean {
+        return false
+    }
+
+    gatherItem(item: MaterialEntity) {
+        item.addToScene(null, null)
+    }
+
+    getDropAction(): RaiderActivity {
+        return RaiderActivity.Place
     }
 
 }
