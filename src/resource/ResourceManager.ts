08a158b5dd2845f34a60e0ff68ce57a6f62154c8
Scarabol <scarabol@gmail.com>
2022-04-01 00:04:16 +0200

    Replace wad worker with typed worker and implement fallback

--- src/resource/ResourceManager.ts
+++ src/resource/ResourceManager.ts
@@ -6,49 +6,56 @@ import { getFilename, getPath } from '../core/Util'
 import { AnimationEntityType } from '../game/model/anim/AnimationEntityType'
 import { DEV_MODE } from '../params'
 import { SceneMesh } from '../scene/SceneMesh'
+import { TypedWorker, TypedWorkerFallback, TypedWorkerFrontend } from '../worker/TypedWorker'
 import { AnimEntityLoader } from './AnimEntityLoader'
 import { LWOLoader } from './LWOLoader'
 import { ResourceCache } from './ResourceCache'
 import { InitLoadingMessage } from './wadworker/InitLoadingMessage'
+import { WadSystem } from './wadworker/WadSystem'
 import { WadWorkerMessage } from './wadworker/WadWorkerMessage'
 import { WorkerMessageType } from './wadworker/WorkerMessageType'
 
 export class ResourceManager extends ResourceCache {
-    static worker: Worker = new Worker(new URL('./wadworker/WadWorker', import.meta.url))
+    static worker: TypedWorker<InitLoadingMessage>
+    static {
+        try {
+            this.worker = new TypedWorkerFrontend(new Worker(new URL('./wadworker/WadWorker', import.meta.url)), (msg) => this.onWadLoaderMessage(msg))
+        } catch (e) {
+            console.warn('Could not setup threaded worker!\nUsing fallback to main thread, which might has bad performance.', e)
+            this.worker = new TypedWorkerFallback<InitLoadingMessage>((r) => this.onWadLoaderMessage(r))
+            new WadSystem(this.worker as TypedWorkerFallback<InitLoadingMessage>)
+        }
+    }
 
     static lwoCache: Map<string, SceneMesh> = new Map()
     static tooltipSpriteCache: Map<string, SpriteImage> = new Map()
 
     static startLoadingFromCache() {
-        return this.startLoading(null)
+        return this.worker.sendMessage(null)
     }
 
     static startLoadingFromUrl(wad0Url: string, wad1Url: string) {
-        return this.startLoading(new InitLoadingMessage(wad0Url, wad1Url))
+        return this.worker.sendMessage(new InitLoadingMessage(wad0Url, wad1Url))
     }
 
-    private static startLoading(msg: InitLoadingMessage) {
-        this.worker.onmessage = (event) => {
-            const msg: WadWorkerMessage = event.data
-            if (msg.type === WorkerMessageType.ASSET) {
-                msg.assetNames.forEach((assetName) => this.resourceByName.set(assetName.toLowerCase(), msg.assetObj))
-                msg.sfxKeys?.forEach((sfxKey) => SoundManager.sfxByKey.set(sfxKey, msg.assetObj))
-                this.onAssetLoaded()
-            } else if (msg.type === WorkerMessageType.MSG) {
-                this.onLoadingMessage(msg.text)
-            } else if (msg.type === WorkerMessageType.CFG) {
-                this.configuration = msg.cfg
-                this.loadDefaultCursor().then(() => this.onInitialLoad(msg.totalResources))
-            } else if (msg.type === WorkerMessageType.CACHE_MISS) {
-                this.onCacheMissed()
-            } else if (msg.type === WorkerMessageType.DONE) {
-                this.loadAllCursor().then(() => {
-                    console.log(`Loading of about ${msg.totalResources} assets complete!`)
-                    this.onLoadDone()
-                })
-            }
+    private static onWadLoaderMessage(msg: WadWorkerMessage) {
+        if (msg.type === WorkerMessageType.ASSET) {
+            msg.assetNames.forEach((assetName) => this.resourceByName.set(assetName.toLowerCase(), msg.assetObj))
+            msg.sfxKeys?.forEach((sfxKey) => SoundManager.sfxByKey.set(sfxKey, msg.assetObj))
+            this.onAssetLoaded()
+        } else if (msg.type === WorkerMessageType.MSG) {
+            this.onLoadingMessage(msg.text)
+        } else if (msg.type === WorkerMessageType.CFG) {
+            this.configuration = msg.cfg
+            this.loadDefaultCursor().then(() => this.onInitialLoad(msg.totalResources))
+        } else if (msg.type === WorkerMessageType.CACHE_MISS) {
+            this.onCacheMissed()
+        } else if (msg.type === WorkerMessageType.DONE) {
+            this.loadAllCursor().then(() => {
+                console.log(`Loading of about ${msg.totalResources} assets complete!`)
+                this.onLoadDone()
+            })
         }
-        this.worker.postMessage(msg)
     }
 
     static onLoadingMessage: (msg: string) => any = (msg: string) => {
