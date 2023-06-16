e3f79ebc4618eacb79ca920aaec9454cf39348d3
Scarabol <scarabol@gmail.com>
2023-06-17 00:47:32 +0200

    Clean code: Merge OffscreenSystem into AbstractGuiSystem

--- src/gui/AbstractGuiSystem.ts
+++ src/gui/AbstractGuiSystem.ts
@@ -7,17 +7,28 @@ import { GameWheelEvent } from '../event/GameWheelEvent'
 import { ChangeCursor, GuiCommand } from '../event/GuiCommand'
 import { NATIVE_SCREEN_HEIGHT, NATIVE_SCREEN_WIDTH } from '../params'
 import { AnimationFrameScaled } from '../screen/AnimationFrame'
-import { OffscreenSystem } from '../worker/OffscreenSystem'
 import { BaseElement } from './base/BaseElement'
 import { Panel } from './base/Panel'
 import { GuiClickEvent, GuiHoverEvent, GuiReleaseEvent } from './event/GuiEvent'
 import { Cursor } from '../resource/Cursor'
 import { OffscreenCache } from '../worker/OffscreenCache'
+import { TypedWorkerBackend } from '../worker/TypedWorker'
+import { OffscreenWorkerMessage } from '../worker/OffscreenWorkerMessage'
+import { WorkerResponse } from '../worker/WorkerResponse'
+import { WorkerMessageType } from '../resource/wadworker/WorkerMessageType'
+import { WorkerEventResponse } from '../worker/WorkerEventResponse'
+import { WorkerPublishEvent } from '../worker/WorkerPublishEvent'
 
-export abstract class AbstractGuiSystem extends OffscreenSystem {
+export abstract class AbstractGuiSystem {
+    readonly eventListener = new Map<EventKey, ((event: GameEvent) => any)[]>()
+    canvas: HTMLCanvasElement | OffscreenCanvas = null
     animationFrame: AnimationFrameScaled
     rootElement: BaseElement = new BaseElement(null)
     panels: Panel[] = []
+
+    constructor(readonly worker: TypedWorkerBackend<OffscreenWorkerMessage, WorkerResponse>) {
+        this.worker.onMessageFromFrontend = (msg) => this.processMessage(msg)
+    }
 
     onCacheReady() {
         this.animationFrame = new AnimationFrameScaled(this.canvas)
@@ -38,7 +49,8 @@ export abstract class AbstractGuiSystem extends OffscreenSystem {
     }
 
     resizeCanvas(width: number, height: number) {
-        super.resizeCanvas(width, height)
+        this.canvas.width = width
+        this.canvas.height = height
         this.animationFrame.scale(this.canvas.width / NATIVE_SCREEN_WIDTH, this.canvas.height / NATIVE_SCREEN_HEIGHT)
         this.animationFrame.notifyRedraw()
     }
@@ -83,5 +95,63 @@ export abstract class AbstractGuiSystem extends OffscreenSystem {
     handleWheelEvent(event: GameWheelEvent): boolean {
         const context = this.animationFrame?.context
         return context && context.getImageData(event.canvasX, event.canvasY, 1, 1).data[3] > 0
+    }
+
+    processMessage(msg: OffscreenWorkerMessage): void {
+        if (msg.type === WorkerMessageType.INIT) {
+            this.canvas = msg.canvas
+            OffscreenCache.resourceByName = msg.resourceByName
+            OffscreenCache.configuration = msg.cfg
+            OffscreenCache.offscreenPreferences = msg.currentPreferences
+            this.onCacheReady()
+        } else if (msg.type === WorkerMessageType.RESIZE) {
+            this.resizeCanvas(msg.canvasWidth, msg.canvasHeight)
+        } else if (msg.type === WorkerMessageType.EVENT_POINTER) {
+            const consumed = this.handlePointerEvent(msg.inputEvent as GamePointerEvent)
+            this.sendEventResponse({
+                type: WorkerMessageType.RESPONSE_EVENT,
+                eventId: msg.eventId,
+                eventConsumed: consumed,
+            })
+        } else if (msg.type === WorkerMessageType.EVENT_KEY) {
+            const consumed = this.handleKeyEvent(msg.inputEvent as GameKeyboardEvent)
+            this.sendEventResponse({
+                type: WorkerMessageType.RESPONSE_EVENT,
+                eventId: msg.eventId,
+                eventConsumed: consumed,
+            })
+        } else if (msg.type === WorkerMessageType.EVENT_WHEEL) {
+            const consumed = this.handleWheelEvent(msg.inputEvent as GameWheelEvent)
+            this.sendEventResponse({
+                type: WorkerMessageType.RESPONSE_EVENT,
+                eventId: msg.eventId,
+                eventConsumed: consumed,
+            })
+        } else if (msg.type === WorkerMessageType.RESET) {
+            this.reset()
+        } else if (msg.type === WorkerMessageType.GAME_EVENT) {
+            const event = msg.gameEvent
+            this.eventListener.getOrUpdate(event.eventKey, () => []).forEach((callback) => callback(event))
+        } else if (!this.onProcessMessage(msg)) {
+            console.warn(`Worker (${this.constructor.name}) ignores message of type: ${WorkerMessageType[msg.type]}`)
+        }
+    }
+
+    abstract onProcessMessage(msg: OffscreenWorkerMessage): boolean
+
+    sendResponse(response: WorkerResponse) {
+        this.worker.sendResponse(response)
+    }
+
+    sendEventResponse(response: WorkerEventResponse) {
+        this.sendResponse(response)
+    }
+
+    publishEvent(event: GuiCommand): void {
+        this.sendResponse(new WorkerPublishEvent(event))
+    }
+
+    registerEventListener(eventKey: EventKey, callback: (event: GameEvent) => any): void {
+        this.eventListener.getOrUpdate(eventKey, () => []).push(callback)
     }
 }
