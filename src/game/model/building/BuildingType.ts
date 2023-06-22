50e9ce7fe3bfccb2f276881b212fe147e65fc052
Scarabol <scarabol@gmail.com>
2023-06-22 20:08:56 +0200

    Fix buildings without power path still using primary one

--- src/game/model/building/BuildingType.ts
+++ src/game/model/building/BuildingType.ts
@@ -84,7 +84,7 @@ export class BuildingType {
     }
 
     removePrimaryPowerPath(): this {
-        this.setPrimaryPowerPath = null
+        this.primaryPowerPath = null
         return this
     }
 
