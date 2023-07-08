19dc7929f63474e98f596f1b1c92be12163a282a
Scarabol <scarabol@gmail.com>
2023-07-08 11:46:57 +0200

    Use fast readback context for main menu panel

--- src/menu/MainMenuPanel.ts
+++ src/menu/MainMenuPanel.ts
@@ -1,11 +1,12 @@
 import { MenuPanelCfg } from '../cfg/MenuCfg'
-import { createContext } from '../core/ImageHelper'
+import { createCanvas, createContext } from '../core/ImageHelper'
 import { SpriteContext } from '../core/Sprite'
 import { ResourceManager } from '../resource/ResourceManager'
 import { MainMenuBaseItem } from './MainMenuBaseItem'
 
 export class MainMenuPanel extends MainMenuBaseItem {
-    context: SpriteContext
+    readonly context: SpriteContext
+    readonly readbackContext: SpriteContext
 
     constructor(cfg: MenuPanelCfg) {
         super(cfg.rect.x, cfg.rect.y, cfg.rect.w, cfg.rect.h)
@@ -13,14 +14,18 @@ export class MainMenuPanel extends MainMenuBaseItem {
         const imgData = ResourceManager.getImageData(cfg.imgBackground)
         this.context = createContext(imgData.width, imgData.height)
         this.context.putImageData(imgData, 0, 0)
+        this.readbackContext = createCanvas(imgData.width, imgData.height).getContext('2d', {willReadFrequently: true}) as SpriteContext
+        this.readbackContext.putImageData(imgData, 0, 0)
     }
 
     isHovered(sx: number, sy: number): boolean {
-        return super.isHovered(sx, sy) && this.context.getImageData(sx, sy, 1, 1).data[3] > 0
+        return super.isHovered(sx, sy) && this.readbackContext.getImageData(sx, sy, 1, 1).data[3] > 0
     }
 
     draw(context: SpriteContext) {
         super.draw(context)
+        super.draw(this.readbackContext)
         context.drawImage(this.context.canvas, this.x, this.y, this.width, this.height)
+        this.readbackContext.drawImage(this.readbackContext.canvas, this.x, this.y, this.width, this.height)
     }
 }
