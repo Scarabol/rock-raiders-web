50b2a69c0ffe1b57b71e73b066e9f4936d19bee8
Scarabol <scarabol@gmail.com>
2024-01-01 20:48:34 +0100

    Fix console logging not intercepted when debugging mobile devices locally

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,9 @@ import '../site/main.css'
 import { DEV_MODE } from './params'
 import { DebugHelper } from './screen/DebugHelper'
 
-if (!DEV_MODE) DebugHelper.intersectConsoleLogging()
-else console.warn('DEV MODE ACTIVE')
+DebugHelper.intersectConsoleLogging()
+
+if (DEV_MODE) console.warn('DEV MODE ACTIVE')
 console.log(`Rock Raider Web v${APP_VERSION}`)
 
 console.time('Total asset loading time')
