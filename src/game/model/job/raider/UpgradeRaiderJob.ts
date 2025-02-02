d209ce106deaf08e9120008b9b86dca4385f8bcb
Scarabol <scarabol@gmail.com>
2025-02-02 14:43:37 +0100

    Make raider move away from building after upgrade like training

--- src/game/model/job/raider/UpgradeRaiderJob.ts
+++ src/game/model/job/raider/UpgradeRaiderJob.ts
@@ -7,6 +7,8 @@ import { BubblesCfg } from '../../../../cfg/BubblesCfg'
 import { HealthComponent } from '../../../component/HealthComponent'
 import { GameConfig } from '../../../../cfg/GameConfig'
 import { DEV_MODE } from '../../../../params'
+import { SurfaceType } from '../../../terrain/SurfaceType'
+import { MoveJob } from '../MoveJob'
 
 export class UpgradeRaiderJob extends RaiderJob {
     building: BuildingEntity
@@ -29,6 +31,15 @@ export class UpgradeRaiderJob extends RaiderJob {
         if (this.raider.level < this.raider.stats.maxLevel) {
             this.raider.level++
             this.raider.worldMgr.ecs.getComponents(this.raider.entity).get(HealthComponent).rockFallDamage = GameConfig.instance.getRockFallDamage(this.raider.entityType, this.raider.level)
+            if (!this.raider.followUpJob && this.building) {
+                const pathSurface = this.building.primaryPathSurface?.neighbors.find((n) => n.surfaceType === SurfaceType.POWER_PATH)
+                if (pathSurface) {
+                    this.raider.followUpJob = new MoveJob(pathSurface.getRandomPosition())
+                } else {
+                    const walkableSurface = this.building.primaryPathSurface?.neighbors.find((n) => n.isWalkable())
+                    if (walkableSurface) this.raider.followUpJob = new MoveJob(walkableSurface.getRandomPosition())
+                }
+            }
         }
     }
 
