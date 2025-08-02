696a52083fbda2f3fc10163de6881caf63bcf508
Scarabol <scarabol@gmail.com>
2025-08-02 08:47:15 +0200

    Fix #42 reduce raider carry rubble animation speed with workaround

--- src/scene/AnimationQualityGroup.ts
+++ src/scene/AnimationQualityGroup.ts
@@ -1,11 +1,13 @@
 import { AnimEntityAnimationData, AnimEntityData } from '../resource/AnimEntityParser'
 import { AnimationGroup } from './AnimationGroup'
 import { SceneMesh } from './SceneMesh'
+import { RAIDER_CARRY_SLOWDOWN } from '../params'
 
 export class AnimationQualityGroup extends AnimationGroup {
     constructor(readonly animEntityData: AnimEntityData, animData: AnimEntityAnimationData, onAnimationDone: (() => void) | undefined, durationTimeoutMs: number = 0, onAnimationTrigger: (() => void) | undefined) {
         super(animData.file, onAnimationDone, durationTimeoutMs, onAnimationTrigger)
-        this.animationTransCoef = 1 / (animData.transcoef || 1)
+        const carryRubbleWorkaround = animData.name.equalsIgnoreCase('Activity_CarryRubble') ? 2 / RAIDER_CARRY_SLOWDOWN : 1
+        this.animationTransCoef = 1 / ((animData.transcoef || 1) * carryRubbleWorkaround) // XXX Remove workaround for raider carry rubble animation speed being too fast
         this.animationTriggerTimeMs = Math.round(animData.trigger / 25 * 1000) // TODO Use actual animation speed from LWS
     }
 
