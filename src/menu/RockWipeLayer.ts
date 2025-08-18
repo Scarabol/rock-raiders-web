12d28b5185fc9614bf3616171af37fcd92028d9a
Unrud <unrud@outlook.com>
2025-08-18 08:31:52 +0200

    Transparent background for rock wipe layer

--- src/menu/RockWipeLayer.ts
+++ src/menu/RockWipeLayer.ts
@@ -1,14 +1,95 @@
 import { ScreenLayer } from '../screen/layer/ScreenLayer'
 import { SoundManager } from '../audio/SoundManager'
-import { AmbientLight, DirectionalLight, OrthographicCamera, Scene } from 'three'
+import { AmbientLight, BufferGeometry, DepthTexture, DirectionalLight, Float32BufferAttribute, Mesh, OrthographicCamera, Scene, ShaderMaterial, ShaderMaterialParameters, Texture, UniformsUtils, Vector2, WebGLRenderer, WebGLRenderTarget } from 'three'
 import { degToRad } from 'three/src/math/MathUtils'
 import { clearIntervalSafe } from '../core/Util'
 import { NATIVE_UPDATE_INTERVAL } from '../params'
 import { AnimationGroup } from '../scene/AnimationGroup'
 import { BaseRenderer } from '../screen/BaseRenderer'
 
+class FullScreenShader extends ShaderMaterial {
+    private static camera = new OrthographicCamera(-1, 1, 1, -1, 0, 1)
+    private mesh: Mesh
+
+    constructor(parameters?: ShaderMaterialParameters) {
+        super(parameters)
+        const geometry = new BufferGeometry()
+    		geometry.setAttribute('position', new Float32BufferAttribute([-1, 3, 0, -1, -1, 0, 3, -1, 0 ], 3));
+    		geometry.setAttribute('uv', new Float32BufferAttribute([0, 2, 0, 0, 2, 0 ], 2));
+    		this.mesh = new Mesh(geometry, this)
+    }
+
+    dispose() {
+        this.mesh.geometry.dispose()
+        super.dispose()
+    }
+
+    render(renderer: WebGLRenderer) {
+        renderer.render(this.mesh, FullScreenShader.camera)
+    }
+}
+
+const TransparentBackgroundShader = {
+    name: 'TransparentBackgroundShader',
+    uniforms: {
+        'tDiffuse': { value: null as Texture | null },
+        'tDepth': { value: null as Texture | null },
+    },
+    vertexShader: `
+        varying vec2 vUv;
+        void main() {
+            vUv = uv;
+            gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
+        }
+    `,
+    fragmentShader: `
+        uniform sampler2D tDiffuse;
+        uniform highp sampler2D tDepth;
+        varying vec2 vUv;
+        void main() {
+            gl_FragColor = texture2D(tDiffuse, vUv);
+            if (texture2D(tDepth, vUv).x == 1.0) gl_FragColor.a = 0.0;
+            gl_FragColor = sRGBTransferOETF(gl_FragColor);
+        }
+    `,
+};
+
+class TransparentBackgroundRenderer extends BaseRenderer {
+    renderTarget?: WebGLRenderTarget
+    fsShader?: FullScreenShader
+
+    render() {
+        if (!this.renderTarget) {
+            this.renderTarget = new WebGLRenderTarget(0, 0, {depthTexture: new DepthTexture(0, 0)})
+        }
+        if (!this.fsShader) {
+            const uniforms = UniformsUtils.clone(TransparentBackgroundShader.uniforms)
+            uniforms.tDiffuse.value = this.renderTarget.texture
+            uniforms.tDepth.value = this.renderTarget.depthTexture
+            this.fsShader = new FullScreenShader({...TransparentBackgroundShader, uniforms})
+        }
+        if (!this.renderer) return 
+        const size = this.renderer.getSize(new Vector2())
+        this.renderTarget.setSize(size.width, size.height)
+        this.renderer.setRenderTarget(this.renderTarget)
+        this.renderer.outputColorSpace
+        try {
+            super.render()
+        } finally {
+            this.renderer.setRenderTarget(null)
+        }
+        this.fsShader.render(this.renderer)
+    }
+
+    dispose() {
+        this.fsShader?.dispose()
+        this.renderTarget?.dispose()
+        super.dispose()
+    }
+}
+
 export class RockWipeLayer extends ScreenLayer {
-    readonly renderer: BaseRenderer
+    readonly renderer: TransparentBackgroundRenderer
     readonly scene: Scene
     readonly group: AnimationGroup
     readonly camera: OrthographicCamera
@@ -16,7 +97,7 @@ export class RockWipeLayer extends ScreenLayer {
 
     constructor() {
         super()
-        this.renderer = new BaseRenderer(NATIVE_UPDATE_INTERVAL, this.canvas, {alpha: true})
+        this.renderer = new TransparentBackgroundRenderer(NATIVE_UPDATE_INTERVAL, this.canvas, {alpha: true})
         // Camera
         const aspect = this.canvas.width / this.canvas.height
         this.camera = new OrthographicCamera(-aspect, aspect, 1, -1, 0, 10)
